1b0bc36ca8b82af084756e504ae4a4c7943fcd24
moderatorman <12193288+moderatorman@users.noreply.github.com>
2024-12-18 02:07:14 -0800

    Add OSM config defaults

--- src/main/java/net/oldschoolminecraft/cs/PluginConfig.java
+++ src/main/java/net/oldschoolminecraft/cs/PluginConfig.java
@@ -21,14 +21,14 @@ public class PluginConfig extends Configuration
 
     public void write()
     {
-        generateConfigOption("mobs.chicken.price", 0D);
-        generateConfigOption("mobs.chicken.enabled", false);
+        generateConfigOption("mobs.chicken.price", 7000D);
+        generateConfigOption("mobs.chicken.enabled", true);
 
-        generateConfigOption("mobs.cow.price", 0D);
-        generateConfigOption("mobs.cow.enabled", false);
+        generateConfigOption("mobs.cow.price", 4500D);
+        generateConfigOption("mobs.cow.enabled", true);
 
-        generateConfigOption("mobs.creeper.price", 0D);
-        generateConfigOption("mobs.creeper.enabled", false);
+        generateConfigOption("mobs.creeper.price", 15000D);
+        generateConfigOption("mobs.creeper.enabled", true);
 
         generateConfigOption("mobs.ghast.price", 0D);
         generateConfigOption("mobs.ghast.enabled", false);
@@ -36,35 +36,35 @@ public class PluginConfig extends Configuration
         generateConfigOption("mobs.giant.price", 0D);
         generateConfigOption("mobs.giant.enabled", false);
 
-        generateConfigOption("mobs.monster.price", 0D);
-        generateConfigOption("mobs.monster.enabled", false);
+        generateConfigOption("mobs.monster.price", 69420D);
+        generateConfigOption("mobs.monster.enabled", true);
 
-        generateConfigOption("mobs.pig.price", 0D);
-        generateConfigOption("mobs.pig.enabled", false);
+        generateConfigOption("mobs.pig.price", 5000D);
+        generateConfigOption("mobs.pig.enabled", true);
 
-        generateConfigOption("mobs.pigzombie.price", 0D);
-        generateConfigOption("mobs.pigzombie.enabled", false);
+        generateConfigOption("mobs.pigzombie.price", 15000D);
+        generateConfigOption("mobs.pigzombie.enabled", true);
 
-        generateConfigOption("mobs.sheep.price", 0D);
-        generateConfigOption("mobs.sheep.enabled", false);
+        generateConfigOption("mobs.sheep.price", 8000D);
+        generateConfigOption("mobs.sheep.enabled", true);
 
-        generateConfigOption("mobs.skeleton.price", 0D);
-        generateConfigOption("mobs.skeleton.enabled", false);
+        generateConfigOption("mobs.skeleton.price", 10000D);
+        generateConfigOption("mobs.skeleton.enabled", true);
 
-        generateConfigOption("mobs.slime.price", 0D);
-        generateConfigOption("mobs.slime.enabled", false);
+        generateConfigOption("mobs.slime.price", 7500D);
+        generateConfigOption("mobs.slime.enabled", true);
 
-        generateConfigOption("mobs.spider.price", 0D);
-        generateConfigOption("mobs.spider.enabled", false);
+        generateConfigOption("mobs.spider.price", 6000D);
+        generateConfigOption("mobs.spider.enabled", true);
 
-        generateConfigOption("mobs.squid.price", 0D);
-        generateConfigOption("mobs.squid.enabled", false);
+        generateConfigOption("mobs.squid.price", 4000D);
+        generateConfigOption("mobs.squid.enabled", true);
 
         generateConfigOption("mobs.wolf.price", 0D);
         generateConfigOption("mobs.wolf.enabled", false);
 
-        generateConfigOption("mobs.zombie.price", 0D);
-        generateConfigOption("mobs.zombie.enabled", false);
+        generateConfigOption("mobs.zombie.price", 6000D);
+        generateConfigOption("mobs.zombie.enabled", true);
     }
 
     private void generateConfigOption(String key, Object defaultValue)
